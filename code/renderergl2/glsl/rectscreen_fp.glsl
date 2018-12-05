83a6191a11594038ec1d9a3c475f5ab58d864caa
Angelo Cano <acano@fastmail.fm>
2018-12-05 05:14:38 -0500

    renderer2 rectangle screen program compile error fix

--- code/renderergl2/glsl/rectscreen_fp.glsl
+++ code/renderergl2/glsl/rectscreen_fp.glsl
@@ -1,11 +1,13 @@
 
-// don't include here since it well end up after extra defines added in tr_glsl.c  :  mesa reports error: #extension directive is not allowed in the middle of a shader
+// don't include here since it will end up after extra defines added in tr_glsl.c  :  mesa reports error: #extension directive is not allowed in the middle of a shader
 
 // #extension GL_ARB_texture_rectangle : enable
 
 uniform sampler2DRect backBufferTex;
 
+varying vec4 mvTexCoord[2];
+
 void main()
 {
-	gl_FragColor = texture2DRect(backBufferTex, gl_TexCoord[0].xy);
+	gl_FragColor = texture2DRect(backBufferTex, mvTexCoord[0].xy);
 }
--- code/renderergl2/glsl/rectscreen_vp.glsl
+++ code/renderergl2/glsl/rectscreen_vp.glsl
@@ -1,5 +1,5 @@
 
-// don't include here since it well end up after extra defines added in tr_glsl.c  :  mesa reports error: #extension directive is not allowed in the middle of a shader
+// don't include here since it will end up after extra defines added in tr_glsl.c  :  mesa reports error: #extension directive is not allowed in the middle of a shader
 
 //#extension GL_ARB_texture_rectangle : enable
 
@@ -7,9 +7,10 @@ attribute vec3 attr_Position;
 attribute vec4 attr_TexCoord0;
 
 uniform mat4   u_ModelViewProjectionMatrix;
+varying vec4 mvTexCoord[2];
 
 void main()
 {
 	gl_Position = u_ModelViewProjectionMatrix * vec4(attr_Position, 1.0);
-	gl_TexCoord[0] = attr_TexCoord0;
+	mvTexCoord[0] = attr_TexCoord0;
 }
